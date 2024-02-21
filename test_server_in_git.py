0e245dd3b2a9a71309938b378ea060abc0facbfb
chaim <LIFE241241@GMAIL.COM>
2024-02-21 17:59:26 +0200

    Add mor function to chek csv
    Make new functions to chek the csv file, if this is correct.

--- test_server_in_git.py
+++ test_server_in_git.py
@@ -9,34 +9,86 @@ from threading import Thread
 
 file_to_chek = 'test2.csv'
 
-with open ('test2.csv', 'r' ,newline="") as filecsv:
+with open (r"C:\Users\LIFE2\Desktop\mego\.py\class 1\test2.py\test2.csv", 'r' ,newline="") as filecsv:
+# with open (r"C:\Users\LIFE2\Desktop\mego\.py\class 1\test2.py\db.csv", 'r' ,newline="") as filecsv:
+
+# with open (r"C:\Users\LIFE2\Desktop\mego\.py\class 1\test2.py\db.csv", 'r' ,newline="") as filecsv:
             list_clients = []
 
             reader = csv.reader(filecsv)
-            total_value = 0
             for row in reader:
-                for i in range(len(row)):
-                    if row[i] == '':
-                        row[i] = 'No value'
-                        total_value += 1 
-                    for j in range(len(list_clients)):
-                        if row[2] == list_clients[j][2]:
-                            list_clients[j][5] = str(int(list_clients[j][5]) + int(row[5]))
-                            row = None
-                            break
-                    if row is None:
-                        break
-                if row is not None:
-                    list_clients.append(row)
+                list_clients.append(row)
             list_clients.sort(key=lambda x: int(x[5]),reverse=True)
-            # print(list_of_all_clients)
-            # for row1 in list_clients:
-            #     a = " ".join(row1)
-            #     list_clients.append(a)
-                # print(row1)
-print(list_clients)
-print (f"total of no value:{total_value}")
 
+
+class Test_of_list_csv:
+    def __init__(self,file_csv):
+        self.file = file_csv
+    
+    def chek_len_id(self,file):
+        total_errors = 0
+        for i in file:
+            if len(i[2]) != 9:
+                # print(f"The id {i[2]} of {i[0]} {i[1]} is not correct!")
+                total_errors +=1
+        return total_errors
+            
+    def chek_len_phone(self,file):
+        total_errors = 0
+        for i in file:
+            if len(i[3]) != 10:
+                # print(f"The number {i[3]} of {i[0]} {i[1]} is not correct!")
+                total_errors +=1
+        return total_errors
+
+    def chek_no_value(self,file):
+        num = 0
+        total_errors = 0
+        for row in file:
+            num += 1
+            for i in range(len(row)):
+                if row[i] == '':
+                    # print(f"it's no value in line {num}")
+                    total_errors +=1
+        return total_errors
+
+    def chek_name_id(self,file):
+        total_errors = 0
+        for i in range(len(file)):
+            for j in range(i+1,len(file)):
+                if file[i][2] == file[j][2]:
+                    # print("The id is not the same value on the names")
+                    total_errors  += 1
+                    # check_name(file[i][0],file[i][1],file[j][0],file[j][1])
+                    break
+        return total_errors
+    
+    def total_dept(self,file):
+        list_dept = []
+        for k in file:
+            if len(list_dept) == 0:
+                list_dept.append(k)
+                continue
+            for t in list_dept:
+                if k[2] == t[2]:
+                    t[5] = str(int(t[5]) + int(k[5]))
+                    break
+                if list_dept[-1] == t:
+                    list_dept.append(k)
+                    break
+                
+        return list_dept
+
+
+test = Test_of_list_csv(list_clients)
+a = test.chek_len_id(list_clients)
+b = test.chek_len_phone(list_clients)
+c = test.chek_no_value(list_clients)
+d = test.chek_name_id(list_clients)
+e = test.total_dept(list_clients)
+# print(list_clients)
+list_clients = e
+print(f"Deitails to fix:\n {a} id's is not correct,\n {b} phon's is not correct,\n {c} value is missing,\n {d} id's is not the same names.")
 
 
 
@@ -58,19 +110,6 @@ class File:
             writer = csv.writer(csvfile)
             data = [data]
             writer.writerow(data)
-        
-    def sort_dept(self):
-        with open(self.filename, "r", newline="") as csvfile:
-            list_of_all_client = []
-            reader = csv.reader(csvfile)
-
-        # קריאה שורה אחר שורה
-            for row in reader:
-                list_of_all_client.append(row)
-
-                list_of_all_client.sort(key=lambda x: int(x[4]))
-                # print(f"id number: {row[2]},dept: {row[4]}")
-        return [row[2],row[4]]
     
 
 
@@ -102,23 +141,6 @@ class File:
 
     
 
-    def sort_name(self):
-        with open(self.filename,'r',newline="") as csvfile:
-            list_of_all_clients = []
-            list_of_all_clients1 = []
-            reader = csv.reader(csvfile)
-            for row in reader:
-                list_of_all_clients.append(row)
-                list_of_all_clients.sort(key=lambda x: str(x[0]))
-            # print(list_of_all_clients)
-            for row1 in list_of_all_clients:
-                a = " ".join(row1)
-                list_of_all_clients1.append(a)
-                # print(row1)
-        print(list_of_all_clients1)
-        return list_of_all_clients1
-    
-
     def sort_name1(self,value,list_client):
         list_client_name = []
         # value = "".join(value)
@@ -127,6 +149,30 @@ class File:
                 list_client_name.append(row)
         list_client_name.sort(key=lambda x: int(x[5]),reverse=True)
         return list_client_name
+    
+
+    def sort_id(self,value,list_client):
+        list_client_id = []
+        for row in list_client:
+            if row[2] == value:
+                list_client_id.append(row)
+        return list_client_id
+    
+
+    def sort_last_name(self,value,list_client):
+        list_client_name = []
+        # value = "".join(value)
+        for row in list_client:
+            if row[1] == value:
+                list_client_name.append(row)
+        # list_client_name.sort(key=lambda x: int(x[5]),reverse=True)
+        return list_client_name
+    def sort_phone(self,value,list_client):
+        list_phone = []
+        for row in list_client:
+            if row[3] == value:
+                list_phone.append(row)
+        return list_phone
         
 
 
@@ -141,13 +187,22 @@ def handle_connection(socket, server_socket, file):
         print(data2)
         # data2 = " ".join(data2)
         # data2 = 'test'
-        if data2[0] == "select" and data2[1] == "dept":
-            return_data2 = file.sort_dept1(data2[2],list_clients)
-            print(return_data2)
-            # data2 = " ".join(data2)
-        
-        elif data2[0] == "select" and data2[1] == "first" and data2[2] == "name":
-            return_data2 = file.sort_name1(data2[3],list_clients)
+        if data2[0] == "select":
+            if data2[1] == "dept":
+                return_data2 = file.sort_dept1(data2[2],list_clients)
+            elif data2[1] == "first" and data2[2] == "name":
+                return_data2 = file.sort_name1(data2[3],list_clients)
+            elif data2[1] == "last" and data2[2] == "name":
+                return_data2 = file.sort_last_name(data2[3],list_clients)
+            elif data2[1] == "id":
+                return_data2 = file.sort_id(data2[2],list_clients)
+            elif data2[1] == "phone":
+                return_data2 = file.sort_phone(data2[2],list_clients)
+        else:
+            return_data2 = ["Enter again your search!"]
+        print(return_data2)
+
+
 
         return_data2 = "\n".join([" ".join(item) for item in return_data2])
 
